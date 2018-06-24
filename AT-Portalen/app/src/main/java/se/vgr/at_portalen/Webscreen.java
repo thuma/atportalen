edde550d0a0c4d4b36ead1cbd4acb508f5db5344
Martin Harari Thuresson <martin@46elks.com>
2018-06-24 12:13:26 +0200

    Added support for file download.

--- AT-Portalen/app/src/main/java/se/vgr/at_portalen/Webscreen.java
+++ AT-Portalen/app/src/main/java/se/vgr/at_portalen/Webscreen.java
@@ -1,9 +1,16 @@
 package se.vgr.at_portalen;
 
+import android.app.DownloadManager;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.CookieManager;
+import android.webkit.DownloadListener;
+import android.webkit.URLUtil;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class Webscreen extends AppCompatActivity {
     WebView webView;
@@ -24,8 +31,30 @@ public class Webscreen extends AppCompatActivity {
                                 "css = css + '#TopMenu {display:none;} #header {display:none;} #leftcol {display:block !important; float:none !important;} #rightcol {display:block !important; float:none !important;} #PageContentInner {margin: 0px !important;} #page { padding:0px !important; margin:0px !importnat;min-width: 0 !important;} div.FooterWrapper, #footerInner { min-width: 0px !important} #centercol {width: 96% !important; margin:0px !important} .RightblockEditorContent { position: inherit !important; }'; " +
                                 "head = document.head; style = document.createElement('style'); style.type = 'text/css'; style.appendChild(document.createTextNode(css)); head.appendChild(style);"
                 );
-            }
+             }
+        });
 
+        webView.setDownloadListener(new DownloadListener() {
+
+            @Override
+            public void onDownloadStart(String url, String userAgent,
+                                        String contentDisposition, String mimetype,
+                                        long contentLength) {
+                DownloadManager.Request request = new DownloadManager.Request(
+                        Uri.parse(url));
+                final String filename = URLUtil.guessFileName(url, contentDisposition, mimetype);
+
+                String cookies = CookieManager.getInstance().getCookie(url);
+                request.addRequestHeader("cookie", cookies);
+                request.addRequestHeader("User-Agent", userAgent);
+                request.allowScanningByMediaScanner();
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED); //Notify client once download is completed!
+                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, filename);
+                DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                dm.enqueue(request);
+                Toast.makeText(getApplicationContext(), "Hämtar fil", //To notify the Client that the file is being downloaded
+                        Toast.LENGTH_LONG).show();
+            }
         });
 
         webView.loadUrl("https://epiintra.vgregion.se/su/at");
